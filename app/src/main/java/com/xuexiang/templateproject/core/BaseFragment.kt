a77418519c9a28cb0e1e1f9f0cfdf35d0c6fa32e
xuexiangjys <xuexiangjys@163.com>
2021-12-30 22:38:19 +0800

    Initial commit

---
+++ app/src/main/java/com/xuexiang/templateproject/core/BaseFragment.kt
@@ -0,0 +1,343 @@
+/*
+ * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.xuexiang.templateproject.core
+
+import android.content.res.Configuration
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.umeng.analytics.MobclickAgent
+import com.xuexiang.xpage.base.XPageActivity
+import com.xuexiang.xpage.base.XPageFragment
+import com.xuexiang.xpage.core.PageOption
+import com.xuexiang.xpage.enums.CoreAnim
+import com.xuexiang.xpage.utils.Utils
+import com.xuexiang.xrouter.facade.service.SerializationService
+import com.xuexiang.xrouter.launcher.XRouter
+import com.xuexiang.xui.utils.WidgetUtils
+import com.xuexiang.xui.widget.actionbar.TitleBar
+import com.xuexiang.xui.widget.actionbar.TitleUtils
+import com.xuexiang.xui.widget.progress.loading.IMessageLoader
+import java.io.Serializable
+import java.lang.reflect.Type
+
+/**
+ * 基础fragment
+ *
+ * @author xuexiang
+ * @since 2018/5/25 下午3:44
+ */
+abstract class BaseFragment<Binding : ViewBinding?> : XPageFragment() {
+    private var mIMessageLoader: IMessageLoader? = null
+    /**
+     * 获取Binding
+     *
+     * @return Binding
+     */
+    /**
+     * ViewBinding
+     */
+    var binding: Binding? = null
+        protected set
+
+    override fun inflateView(inflater: LayoutInflater, container: ViewGroup): View {
+        binding = viewBindingInflate(inflater, container)
+        return binding!!.root
+    }
+
+    /**
+     * 构建ViewBinding
+     *
+     * @param inflater  inflater
+     * @param container 容器
+     * @return ViewBinding
+     */
+    protected abstract fun viewBindingInflate(inflater: LayoutInflater, container: ViewGroup): Binding
+
+    override fun initPage() {
+        initTitle()
+        initViews()
+        initListeners()
+    }
+
+    protected fun initTitle(): TitleBar {
+        return TitleUtils.addTitleBarDynamic(rootView as ViewGroup, pageTitle) { popToBack() }
+    }
+
+    override fun initListeners() {}
+    val messageLoader: IMessageLoader?
+        get() {
+            if (mIMessageLoader == null) {
+                mIMessageLoader = WidgetUtils.getMiniLoadingDialog(requireContext())
+            }
+            return mIMessageLoader
+        }
+
+    fun getMessageLoader(message: String?): IMessageLoader? {
+        if (mIMessageLoader == null) {
+            mIMessageLoader = WidgetUtils.getMiniLoadingDialog(requireContext(), message!!)
+        } else {
+            mIMessageLoader!!.updateMessage(message)
+        }
+        return mIMessageLoader
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        //屏幕旋转时刷新一下title
+        super.onConfigurationChanged(newConfig)
+        val root = rootView as ViewGroup
+        if (root.getChildAt(0) is TitleBar) {
+            root.removeViewAt(0)
+            initTitle()
+        }
+    }
+
+    override fun onDestroyView() {
+        if (mIMessageLoader != null) {
+            mIMessageLoader!!.dismiss()
+        }
+        super.onDestroyView()
+        binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        MobclickAgent.onPageStart(pageName)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        MobclickAgent.onPageEnd(pageName)
+    }
+    //==============================页面跳转api===================================//
+    /**
+     * 打开一个新的页面【建议只在主tab页使用】
+     *
+     * @param clazz 页面的类
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openNewPage(clazz: Class<T>?): Fragment {
+        return PageOption(clazz)
+                .setNewActivity(true)
+                .open(this)
+    }
+
+    /**
+     * 打开一个新的页面【建议只在主tab页使用】
+     *
+     * @param pageName 页面名
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openNewPage(pageName: String?): Fragment {
+        return PageOption(pageName)
+                .setAnim(CoreAnim.slide)
+                .setNewActivity(true)
+                .open(this)
+    }
+
+    /**
+     * 打开一个新的页面【建议只在主tab页使用】
+     *
+     * @param clazz                页面的类
+     * @param containActivityClazz 页面容器
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openNewPage(clazz: Class<T>?, containActivityClazz: Class<out XPageActivity?>): Fragment {
+        return PageOption(clazz)
+                .setNewActivity(true)
+                .setContainActivityClazz(containActivityClazz)
+                .open(this)
+    }
+
+    /**
+     * 打开一个新的页面【建议只在主tab页使用】
+     *
+     * @param clazz 页面的类
+     * @param key   入参的键
+     * @param value 入参的值
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openNewPage(clazz: Class<T>?, key: String?, value: Any?): Fragment {
+        val option = PageOption(clazz).setNewActivity(true)
+        return openPage(option, key, value)
+    }
+
+    fun openPage(option: PageOption, key: String?, value: Any?): Fragment {
+        if (value is Int) {
+            option.putInt(key, (value as Int?)!!)
+        } else if (value is Float) {
+            option.putFloat(key, (value as Float?)!!)
+        } else if (value is String) {
+            option.putString(key, value as String?)
+        } else if (value is Boolean) {
+            option.putBoolean(key, (value as Boolean?)!!)
+        } else if (value is Long) {
+            option.putLong(key, (value as Long?)!!)
+        } else if (value is Double) {
+            option.putDouble(key, (value as Double?)!!)
+        } else if (value is Parcelable) {
+            option.putParcelable(key, value as Parcelable?)
+        } else if (value is Serializable) {
+            option.putSerializable(key, value as Serializable?)
+        } else {
+            option.putString(key, serializeObject(value))
+        }
+        return option.open(this)
+    }
+
+    /**
+     * 打开页面
+     *
+     * @param clazz          页面的类
+     * @param addToBackStack 是否加入回退栈
+     * @param key            入参的键
+     * @param value          入参的值
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openPage(clazz: Class<T>?, addToBackStack: Boolean, key: String?, value: String?): Fragment {
+        return PageOption(clazz)
+                .setAddToBackStack(addToBackStack)
+                .putString(key, value)
+                .open(this)
+    }
+
+    /**
+     * 打开页面
+     *
+     * @param clazz 页面的类
+     * @param key   入参的键
+     * @param value 入参的值
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openPage(clazz: Class<T>?, key: String?, value: Any?): Fragment {
+        return openPage(clazz, true, key, value)
+    }
+
+    /**
+     * 打开页面
+     *
+     * @param clazz          页面的类
+     * @param addToBackStack 是否加入回退栈
+     * @param key            入参的键
+     * @param value          入参的值
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openPage(clazz: Class<T>?, addToBackStack: Boolean, key: String?, value: Any?): Fragment {
+        val option = PageOption(clazz).setAddToBackStack(addToBackStack)
+        return openPage(option, key, value)
+    }
+
+    /**
+     * 打开页面
+     *
+     * @param clazz 页面的类
+     * @param key   入参的键
+     * @param value 入参的值
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openPage(clazz: Class<T>?, key: String?, value: String?): Fragment {
+        return PageOption(clazz)
+                .putString(key, value)
+                .open(this)
+    }
+
+    /**
+     * 打开页面,需要结果返回
+     *
+     * @param clazz       页面的类
+     * @param key         入参的键
+     * @param value       入参的值
+     * @param requestCode 请求码
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openPageForResult(clazz: Class<T>?, key: String?, value: Any?, requestCode: Int): Fragment {
+        val option = PageOption(clazz).setRequestCode(requestCode)
+        return openPage(option, key, value)
+    }
+
+    /**
+     * 打开页面,需要结果返回
+     *
+     * @param clazz       页面的类
+     * @param key         入参的键
+     * @param value       入参的值
+     * @param requestCode 请求码
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openPageForResult(clazz: Class<T>?, key: String?, value: String?, requestCode: Int): Fragment {
+        return PageOption(clazz)
+                .setRequestCode(requestCode)
+                .putString(key, value)
+                .open(this)
+    }
+
+    /**
+     * 打开页面,需要结果返回
+     *
+     * @param clazz       页面的类
+     * @param requestCode 请求码
+     * @param <T>
+     * @return
+    </T> */
+    fun <T : XPageFragment?> openPageForResult(clazz: Class<T>?, requestCode: Int): Fragment {
+        return PageOption(clazz)
+                .setRequestCode(requestCode)
+                .open(this)
+    }
+
+    /**
+     * 序列化对象
+     *
+     * @param object 需要序列化的对象
+     * @return 序列化结果
+     */
+    fun serializeObject(`object`: Any?): String {
+        return XRouter.getInstance().navigation(SerializationService::class.java).object2Json(`object`)
+    }
+
+    /**
+     * 反序列化对象
+     *
+     * @param input 反序列化的内容
+     * @param clazz 类型
+     * @return 反序列化结果
+     */
+    fun <T> deserializeObject(input: String?, clazz: Type?): T {
+        return XRouter.getInstance().navigation(SerializationService::class.java).parseObject(input, clazz)
+    }
+
+    override fun hideCurrentPageSoftInput() {
+        if (activity == null) {
+            return
+        }
+        // 记住，要在xml的父布局加上android:focusable="true" 和 android:focusableInTouchMode="true"
+        Utils.hideSoftInputClearFocus(requireActivity().currentFocus)
+    }
+}
---
+++ app/src/main/java/com/xuexiang/templateproject/utils/sdkinit/UMengInit.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.xuexiang.templateproject.utils.sdkinit
+
+import android.app.Application
+import android.content.Context
+import com.meituan.android.walle.WalleChannelReader
+import com.umeng.analytics.MobclickAgent
+import com.umeng.commonsdk.UMConfigure
+import com.xuexiang.templateproject.BuildConfig
+import com.xuexiang.templateproject.MyApp
+
+/**
+ * UMeng 统计 SDK初始化
+ *
+ * @author xuexiang
+ * @since 2019-06-18 15:49
+ */
+class UMengInit private constructor() {
+    companion object {
+        private const val DEFAULT_CHANNEL_ID = "github"
+
+        /**
+         * 初始化SDK,合规指南【先进行预初始化，如果用户隐私同意后可以初始化UmengSDK进行信息上报】
+         */
+        fun init(context: Context) {
+            val appContext = context.applicationContext
+            if (appContext is Application) {
+                initApplication(appContext)
+            }
+        }
+
+        /**
+         * 初始化SDK,合规指南【先进行预初始化，如果用户隐私同意后可以初始化UmengSDK进行信息上报】
+         *
+         * @param application 应用上下文
+         */
+        private fun initApplication(application: Application) {
+            // 运营统计数据调试运行时不初始化
+            if (MyApp.isDebug) {
+                return
+            }
+            UMConfigure.setLogEnabled(false)
+            UMConfigure.preInit(application, BuildConfig.APP_ID_UMENG, getChannel(application))
+            // 用户同意了隐私协议
+            if (isAgreePrivacy) {
+                realInit(application)
+            }
+        }// TODO: 2021/5/11 隐私协议设置
+
+        /**
+         * @return 用户是否同意了隐私协议
+         */
+        private val isAgreePrivacy: Boolean
+            private get() =// TODO: 2021/5/11 隐私协议设置
+                true
+
+        /**
+         * 真实的初始化UmengSDK【进行设备信息的统计上报，必须在获得用户隐私同意后方可调用】
+         */
+        private fun realInit(application: Application) {
+            // 运营统计数据调试运行时不初始化
+            if (MyApp.isDebug) {
+                return
+            }
+            //初始化组件化基础库, 注意: 即使您已经在AndroidManifest.xml中配置过appkey和channel值，也需要在App代码中调用初始化接口（如需要使用AndroidManifest.xml中配置好的appkey和channel值，UMConfigure.init调用中appkey和channel参数请置为null）。
+            //第二个参数是appkey，最后一个参数是pushSecret
+            //这里BuildConfig.APP_ID_UMENG是根据local.properties中定义的APP_ID_UMENG生成的，只是运行看效果的话，可以不初始化该SDK
+            UMConfigure.init(application, BuildConfig.APP_ID_UMENG, getChannel(application), UMConfigure.DEVICE_TYPE_PHONE, "")
+            //统计SDK是否支持采集在子进程中打点的自定义事件，默认不支持
+            //支持多进程打点
+            UMConfigure.setProcessEvent(true)
+            MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO)
+        }
+
+        /**
+         * 获取渠道信息
+         */
+        private fun getChannel(context: Context): String? {
+            return WalleChannelReader.getChannel(context, DEFAULT_CHANNEL_ID)
+        }
+    }
+
+    init {
+        throw UnsupportedOperationException("u can't instantiate me...")
+    }
+}
